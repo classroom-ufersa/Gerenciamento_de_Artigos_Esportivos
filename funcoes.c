775cc7418de5734597e071137a9d256a2229eb7e
DanielNeres <danielnerres@gmail.com>
2024-03-20 10:36:47 -0300

    Criação da função menu

---
+++ funcoes.c
@@ -0,0 +1,19 @@
+#include "funcoes.h"
+#include <stdio.h>
+
+
+void menu(){
+
+    char escolha;
+    scanf("%c", escolha);
+
+    switch (escolha)
+    {
+    case '1':
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
