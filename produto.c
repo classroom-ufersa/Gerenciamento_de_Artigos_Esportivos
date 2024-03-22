8f9fdce65d1d3d9c8aa0d8c9f88b51cd5ff6ee98
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2024-03-22 15:19:57 -0300

    Função adicionar produto

--- produto.c
+++ produto.c
@@ -1,1 +1,15 @@
+#include "produto.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+void adicionarProduto(Lista_Produtos *lista, Produto produto) {
+    No_Produto *novoNo = (No_Produto*)malloc(sizeof(No_Produto));
+    if (novoNo != NULL) {
+        novoNo->produto = produto;
+        novoNo->proximo = lista->primeiro;
+        lista->primeiro = novoNo;
+    } else {
+        printf("Erro: Não foi possível adicionar o produto. Memória insuficiente.\n");
+    }
+}
