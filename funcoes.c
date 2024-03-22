6e336eaeec287af9d33586715871321ae4af213b
DanielNeres <danielnerres@gmail.com>
2024-03-22 15:29:52 -0300

    Modificação na função listar_memu

--- funcoes.c
+++ funcoes.c
@@ -3,16 +3,16 @@
 
 char exibirMenu() {
     char escolha;
-    printf("\nMenu:\n");
-    printf("1. Adicionar produto\n");
-    printf("2. Remover produto\n");
-    printf("3. Adicionar sessao\n");
-    printf("4. Remover sessao\n");
-    printf("5. Editar informações do produto\n");
-    printf("6. Buscar produto por nome\n");
-    printf("7. Listar produtos por nome\n");
-    printf("8. Sair\n");
-    printf("Escolha uma opção (1-8): ");
+    printf("\n\t\033[34m=== Menu ===\n");
+    printf("\t\033[1;33m1.\033[0m Adicionar produto\n");
+    printf("\t\033[1;33m2.\033[0m Remover produto\n");
+    printf("\t\033[1;33m3.\033[0m Adicionar sessão\n");
+    printf("\t\033[1;33m4.\033[0m Remover sessão\n");
+    printf("\t\033[1;33m5.\033[0m Editar informações do produto\n");
+    printf("\t\033[1;33m6.\033[0m Buscar produto por nome\n");
+    printf("\t\033[1;33m7.\033[0m Listar produtos por nome\n");
+    printf("\t\033[1;33m8.\033[0m Sair\n");
+    printf("\tEscolha uma opção (\033[1;33m1-8\033[0m): ");
     scanf("%c", &escolha);
     return escolha;
 }
