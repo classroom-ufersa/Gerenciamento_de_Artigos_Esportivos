9d916e2f9c3c982ba36c63d9aa5e694640afaa9e
DanielNeres <danielnerres@gmail.com>
2024-04-09 14:39:28 -0300

    Comentando  funções.h

--- C/Funcoes/funcoes.c
+++ C/Funcoes/funcoes.c
@@ -7,13 +7,13 @@ char exibirMenu() {
     printf("\n\t\033[34m=== Menu ===\n");
     printf("\t\033[1;33m1.\033[0m Adicionar produto\n");
     printf("\t\033[1;33m2.\033[0m Remover produto\n");
-    printf("\t\033[1;33m3.\033[0m Adicionar sess�o\n");
-    printf("\t\033[1;33m4.\033[0m Remover sess�o\n");
-    printf("\t\033[1;33m5.\033[0m Editar informa��es do produto\n");
+    printf("\t\033[1;33m3.\033[0m Adicionar sessão\n");
+    printf("\t\033[1;33m4.\033[0m Remover sessão\n");
+    printf("\t\033[1;33m5.\033[0m Editar informações do produto\n");
     printf("\t\033[1;33m6.\033[0m Buscar produto por nome\n");
     printf("\t\033[1;33m7.\033[0m Listar produtos por nome\n");
     printf("\t\033[1;33m8.\033[0m Sair\n");
-    printf("\tEscolha uma op��o (\033[1;33m1-8\033[0m): ");
+    printf("\tEscolha uma opção (\033[1;33m1-8\033[0m): ");
     scanf(" %s", escolha);
     if (escolha[1] == '\0'){
         return escolha[0];
@@ -27,7 +27,7 @@ void Escrever_Arquivo(Lista_Sessao *lista_sessao_var){
     Escrever_Sessao(lista_sessao_var, Arquivo);
     if (Arquivo == NULL)
     {
-        printf("errp na abertura para escrever de arquivo\n");
+        printf("erro na abertura para escrever de arquivo\n");
         exit(1);
     }
     fclose(Arquivo);
--- C/Funcoes/funcoes.h
+++ C/Funcoes/funcoes.h
@@ -5,10 +5,54 @@
 #include "../Sessao/sessao.h"
 #include "../Produto/produto.h"
 
+/**
+ * @brief Exibe o menu e retorna a opção escolhida pelo usuário.
+ * 
+ * @return Caractere representando a opção escolhida pelo usuário.
+ */
 char exibirMenu();
+
+/**
+ * @brief Escreve os detalhes das sessões em um arquivo.
+ * 
+ * @param lista_sessao_var Ponteiro para a lista de sessões.
+ */
 void Escrever_Arquivo(Lista_Sessao *lista_sessao_var);
+
+/**
+ * @brief Conta o número de sessões e produtos.
+ * 
+ * @return Um array de inteiros contendo o número de sessões e produtos, respectivamente.
+ */
 int *Contador_Sessoes_e_Produtos();
+
+/**
+ * @brief Lê os detalhes das sessões de um arquivo e as adiciona à lista de sessões.
+ * 
+ * @param lista_sessao_var Ponteiro para a lista de sessões.
+ * @return Ponteiro para a lista de sessões atualizada.
+ */
 Lista_Sessao *Ler_Arquivo(Lista_Sessao *lista_sessao_var);
+
+/**
+ * @brief Converte uma string para maiúsculas ou minúsculas.
+ * 
+ * @param Nome Ponteiro para a string a ser convertida.
+ */
 void string_maiuscula_minuscula(char *Nome);
+
+/**
+ * @brief Extrai um número float de uma string.
+ * 
+ * @param str Ponteiro para a string contendo o número float.
+ * @return O número float extraído da string.
+ */
 float extrairFloat(char *str);
+
+/**
+ * @brief Extrai um número inteiro de uma string.
+ * 
+ * @param str Ponteiro para a string contendo o número inteiro.
+ * @return O número inteiro extraído da string.
+ */
 int extrairInt(char *str);
